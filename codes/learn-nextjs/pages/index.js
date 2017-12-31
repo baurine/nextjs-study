f0b174be24a88aadd9743dcbc4856c6e8af27b99
baurine <2008.hbl@gmail.com>
2017-12-31 15:35:46 +0800

    MISC - Create Dynamic Pages: Use url query params

--- codes/learn-nextjs/pages/index.js
+++ codes/learn-nextjs/pages/index.js
@@ -1,8 +1,24 @@
+import Link from 'next/link'
 import Layout from '../components/MyLayout'
+
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
 const Index = () => (
   <Layout>
-    <p>Hello Next.js</p>
+    <h1>My Blog</h1>
+    {
+      [
+        'Hello Next.js',
+        'Learn Next.js is awesome',
+        'Deploy apps with Zeit'
+      ].map(title => <PostLink title={title} />)
+    }
   </Layout>
 )
 
